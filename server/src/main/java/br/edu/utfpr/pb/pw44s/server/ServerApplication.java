2f057887aee6d752b0bdb5900bb152e6638b8120
Vinicius Pegorini <viniciuspegorini@gmail.com>
2025-09-08 20:42:59 -0300

    Aula do dia 08/09/2025. Desenvolvimento e configuração da camada de segurança da API.

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/ServerApplication.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/ServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ServerApplication {
+
+    //https://github.com/viniciuspegorini/pw44s-2025-2
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
